1d797778eab3b002c091b41998bae90195295edf
Joseph C Wang <joequant@gmail.com>
2015-12-27 17:15:23 +0800

    fix repaint

--- test.js
+++ test.js
@@ -1,31 +1,44 @@
 // Look ma, no jQuery!
 var pdf_file = 'pdfjs/web/compressed.tracemonkey-pldi-09.pdf';
-LC.init(
+
+var lc = LC.init(
     document.getElementsByClassName('literally')[0],
     {imageURLPrefix: 'literallycanvas/img'}
 );
 
-function draw_pdf(pdf_file, pageno) {
-    PDFJS.getDocument(pdf_file).then(function(pdf) {
+PDFJS.getDocument(pdf_file).then(function(pdf) {
+    function draw_pdf(pageno) {
 	// you can now use *pdf* here
 	pdf.getPage(pageno).then(function(page) {
 	    // you can now use *page* here
 	    var scale = 1.5;
 	    var viewport = page.getViewport(scale);
+	    var i, canvas_list =
+		document.querySelectorAll('div.lc-drawing canvas');
+	    for (i=0; i < canvas_list.length; ++i) {
+	    	canvas_list[i].style.height = viewport.height;
+		canvas_list[i].style.width = viewport.width;
+		canvas_list[i].height = viewport.height;
+		canvas_list[i].width = viewport.width;
+	    }
 	    var canvas =
 		document.querySelector('div.lc-drawing canvas:first-child');
 	    var context = canvas.getContext('2d');
-	    canvas.style.height = viewport.height;
-	    canvas.style.width = viewport.width;
-	    canvas.height = viewport.height;
-	    canvas.width = viewport.width;
 	    var renderContext = {
 		canvasContext: context,
 		viewport: viewport
 	    };
 	    page.render(renderContext);
 	});
+    };
+    lc.on("repaint", function(layer) {
+	if (layer.layerKey != "background") {
+	    return;
+	}
+	draw_pdf(1);
     });
-}
+    draw_pdf(1);
+});
 
-draw_pdf(pdf_file, 1);
+
+
