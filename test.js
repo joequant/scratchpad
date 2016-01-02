7298efa73f295d63b375ad6e866a5f779b03613b
Joseph C Wang <joequant@gmail.com>
2016-01-02 21:48:20 +0800

    change pages

--- test.js
+++ test.js
@@ -17,29 +17,52 @@ var draw_pdf = function() {};
 
 PDFJS.getDocument(pdf_file).then(function(pdf) {
     var num_pages = pdf.numPages;
+    var current_page = 1;
     document.getElementById('numPages').textContent = num_pages;
     document.getElementById('pageNumber').max = num_pages;
-    pdf.getPage(pageno).then (
-	function(page) {
-	    draw_pdf = function() {
-		// you can now use *page* here
-		var viewport = page.getViewport(lc.scale);
-		var i;
-		for (i=0; i < canvas_list.length; ++i) {
-		    canvas_list[i].style.height = viewport.height;
-		    canvas_list[i].style.width = viewport.width;
-		    canvas_list[i].height = viewport.height;
-		    canvas_list[i].width = viewport.width;
-		}
-		var renderContext = {
-		    canvasContext: context,
-		    viewport: viewport
+    function set_drawpdf(pageno) {
+	document.getElementById('pageNumber').value = pageno;
+	pdf.getPage(pageno).then (
+	    function(page) {
+		draw_pdf = function() {
+		    // you can now use *page* here
+		    var viewport = page.getViewport(lc.scale);
+		    var i;
+		    for (i=0; i < canvas_list.length; ++i) {
+			canvas_list[i].style.height = viewport.height;
+			canvas_list[i].style.width = viewport.width;
+			canvas_list[i].height = viewport.height;
+			canvas_list[i].width = viewport.width;
+		    }
+		    var renderContext = {
+			canvasContext: context,
+			viewport: viewport
+		    };
+		    page.render(renderContext);
 		};
-		page.render(renderContext);
-	    };
-	    lc.repaintAllLayers();
+		lc.repaintAllLayers();
+	    }
+	);
+    }
+    document.getElementById("previous").onclick = function(event) {
+	if (current_page <= 1) {
+	    current_page = 1;
+	    return;
 	}
-    );
+	current_page = current_page - 1;
+	set_drawpdf(current_page);
+    };
+
+    document.getElementById("next").onclick = function(event) {
+	if (current_page >= num_pages) {
+	    current_page = num_pages;
+	    return;
+	}
+	current_page = current_page + 1;
+	set_drawpdf(current_page);
+    };
+    
+    set_drawpdf(current_page);
 });
 
 lc.on("pan", function(pos) {
